5bc329e2834f0d752c697765f6d0c294fc10bf96
kajalsagar227 <kajalsagar902@gmail.com>
2025-03-15 22:36:09 +0530

    AOS effect has been added.
    Added AOS animations for smooth scrolling effects

--- src/Homepage/Seventh.jsx
+++ src/Homepage/Seventh.jsx
@@ -1,49 +1,48 @@
-import React from 'react'
-import { Card, Col, Container, Form, InputGroup, Row } from 'react-bootstrap'
-import { FaEnvelope, FaUser } from 'react-icons/fa';
-import { IoLocation } from "react-icons/io5";
-import { SlCalender } from 'react-icons/sl';
-import { BsHouseDoorFill } from 'react-icons/bs';
-import { IoIosSettings } from 'react-icons/io';
+import React, { useEffect } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Seventh() {
+    useEffect(() => {
+        AOS.init({ duration: 1000, once: true });
+    }, []);
+
     return (
         <>
             <br /><br /><br /><br />
             <div style={{ height: "auto", backgroundColor: "black", display: "flex", alignItems: "center", padding: "20px 0" }}>
-    <Container>
-        <Row className="text-center d-flex justify-content-center align-items-center flex-wrap">
-            <Col xs={12} md={3} className="d-flex flex-column align-items-center justify-content-center">
-                <Card.Title style={{ fontFamily: "sans-serif", fontWeight: "bold", color: "white", fontSize: "3rem" }}>36</Card.Title>
-                <Card.Text style={{ fontFamily: "sans-serif", color: "white", fontSize: "1.2rem" }}>Countries</Card.Text>
-            </Col>
+                <Container>
+                    <Row className="text-center d-flex justify-content-center align-items-center flex-wrap" data-aos="fade-in">
+                        <Col xs={12} md={3} className="d-flex flex-column align-items-center justify-content-center">
+                            <Card.Title style={{ fontFamily: "sans-serif", fontWeight: "bold", color: "white", fontSize: "3rem" }}>36</Card.Title>
+                            <Card.Text style={{ fontFamily: "sans-serif", color: "white", fontSize: "1.2rem" }}>Countries</Card.Text>
+                        </Col>
 
-            {/* Center Line (Hidden on Small Screens) */}
-            <Col xs="auto" className="d-none d-md-flex align-items-center">
-                <div style={{ height: "80px", width: "2px", backgroundColor: "white" }}></div>
-            </Col>
+                        {/* Center Line (Hidden on Small Screens) */}
+                        <Col xs="auto" className="d-none d-md-flex align-items-center">
+                            <div style={{ height: "80px", width: "2px", backgroundColor: "white" }}></div>
+                        </Col>
 
-            <Col xs={12} md={3} className="d-flex flex-column align-items-center justify-content-center">
-                <Card.Title style={{ fontFamily: "sans-serif", fontWeight: "bold", color: "white", fontSize: "3rem" }}>15 Years</Card.Title>
-                <Card.Text style={{ fontFamily: "sans-serif", color: "white", fontSize: "1.2rem" }}>of Experience</Card.Text>
-            </Col>
+                        <Col xs={12} md={3} className="d-flex flex-column align-items-center justify-content-center">
+                            <Card.Title style={{ fontFamily: "sans-serif", fontWeight: "bold", color: "white", fontSize: "3rem" }}>15 Years</Card.Title>
+                            <Card.Text style={{ fontFamily: "sans-serif", color: "white", fontSize: "1.2rem" }}>of Experience</Card.Text>
+                        </Col>
 
-            {/* Center Line (Hidden on Small Screens) */}
-            <Col xs="auto" className="d-none d-md-flex align-items-center">
-                <div style={{ height: "80px", width: "2px", backgroundColor: "white" }}></div>
-            </Col>
+                        {/* Center Line (Hidden on Small Screens) */}
+                        <Col xs="auto" className="d-none d-md-flex align-items-center">
+                            <div style={{ height: "80px", width: "2px", backgroundColor: "white" }}></div>
+                        </Col>
 
-            <Col xs={12} md={3} className="d-flex flex-column align-items-center justify-content-center">
-                <Card.Title style={{ fontFamily: "sans-serif", fontWeight: "bold", color: "white", fontSize: "3rem" }}>950</Card.Title>
-                <Card.Text style={{ fontFamily: "sans-serif", color: "white", fontSize: "1.2rem" }}>Airlines</Card.Text>
-            </Col>
-        </Row>
-    </Container>
-</div>
-
+                        <Col xs={12} md={3} className="d-flex flex-column align-items-center justify-content-center">
+                            <Card.Title style={{ fontFamily: "sans-serif", fontWeight: "bold", color: "white", fontSize: "3rem" }}>950</Card.Title>
+                            <Card.Text style={{ fontFamily: "sans-serif", color: "white", fontSize: "1.2rem" }}>Airlines</Card.Text>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </>
-
-    )
+    );
 }
 
-export default Seventh
+export default Seventh;
